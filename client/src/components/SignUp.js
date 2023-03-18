309ea0561f087815c7866f865883ac7ed6f1bf3f
Pouya <zarepouya7@gmail.com>
2023-03-18 13:19:52 +0200

    add fetch api for signup and final ui

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,56 +1,149 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import axios from "axios";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
 function SignUp() {
+  const navigate = useNavigate();
+  const [error, seterror] = useState("");
   const [data, setdata] = useState({
-    firstName:"",
-    lastName:"",
-    email:"",
-    password:""
-  })
-  const handleChange = ({currentTarget: input}) => {
-    setdata({...data,[input.name]:input.value});
-  }
-  const handleSubmit = (e) => {
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+  const handleChange = ({ currentTarget: input }) => {
+    setdata({ ...data, [input.name]: input.value });
+  };
+  const handleSubmit = async (e) => {
     e.preventDefault();
-  }
+    try {
+      const url = "http://localhost:8040/api/users";
+      const { data: res } = await axios.post(url);
+      navigate("/login");
+      console.log(res.message);
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.status >= 400 &&
+        error.response.status <= 500
+      ) {
+        seterror(error.response.data.message);
+      }
+    }
+  };
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
-  <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-      <h2 className="flex items-center mb-6 text-2xl font-semibold text-gray-900 text-2xl text-blue-600">
-         Fullstack-project   
-      </h2>
-      <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <h2 className="flex items-center mb-6 text-2xl font-semibold text-gray-900 text-2xl text-blue-600">
+          Fullstack-project
+        </h2>
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
-                  Create and account
-              </h1>
-              <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
-                  <div>
-                      <label for="firstName" className="block mb-2 text-sm font-medium text-gray-900 ">Your FirstName</label>
-                      <input type="text" onChange={handleChange} value={data.firstName} name="firstName" id="firstName" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " placeholder="John" required=""/>
-                  </div>
-                  <div>
-                      <label for="lastName" className="block mb-2 text-sm font-medium text-gray-900 ">Your LastName</label>
-                      <input type="text" onChange={handleChange} value={data.lastName} name="lastName" id="lastName" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " placeholder="Smith" required=""/>
-                  </div>
-                  <div>
-                      <label for="email" className="block mb-2 text-sm font-medium text-gray-900 ">Your email</label>
-                      <input type="email" onChange={handleChange} value={data.email} name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " placeholder="johnsmith@company.com" required=""/>
-                  </div>
-                  <div>
-                      <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                      <input type="password" onChange={handleChange} value={data.password} name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " required=""/>
-                  </div>
-                  <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 bg-blue-600 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Create an account</button>
-                  <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                      Already have an account? <Link to='/login' className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login here</Link>
-                  </p>
-              </form>
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
+              Create and account
+            </h1>
+            <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+              <div>
+                <label
+                  for="firstName"
+                  className="block mb-2 text-sm font-medium text-gray-900 "
+                >
+                  Your FirstName
+                </label>
+                <input
+                  type="text"
+                  onChange={handleChange}
+                  value={data.firstName}
+                  name="firstName"
+                  id="firstName"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                  placeholder="John"
+                  required=""
+                />
+              </div>
+              <div>
+                <label
+                  for="lastName"
+                  className="block mb-2 text-sm font-medium text-gray-900 "
+                >
+                  Your LastName
+                </label>
+                <input
+                  type="text"
+                  onChange={handleChange}
+                  value={data.lastName}
+                  name="lastName"
+                  id="lastName"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                  placeholder="Smith"
+                  required=""
+                />
+              </div>
+              <div>
+                <label
+                  for="email"
+                  className="block mb-2 text-sm font-medium text-gray-900 "
+                >
+                  Your email
+                </label>
+                <input
+                  type="email"
+                  onChange={handleChange}
+                  value={data.email}
+                  name="email"
+                  id="email"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                  placeholder="johnsmith@company.com"
+                  required=""
+                />
+              </div>
+              <div>
+                <label
+                  for="password"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                  onChange={handleChange}
+                  value={data.password}
+                  name="password"
+                  id="password"
+                  placeholder="••••••••"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
+                  required=""
+                />
+              </div>
+              {error && (
+                <div
+                  class="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4"
+                  role="alert"
+                >
+                  <p class="font-bold">{error}</p>
+                </div>
+              )}
+              <button
+                type="submit"
+                className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 bg-blue-600 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
+              >
+                Create an account
+              </button>
+              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                Already have an account?{" "}
+                <Link
+                  to="/login"
+                  className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                >
+                  Login here
+                </Link>
+              </p>
+            </form>
           </div>
+        </div>
       </div>
-  </div>
-</section>
-  )
+    </section>
+  );
 }
 
-export default SignUp
+export default SignUp;
